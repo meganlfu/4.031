b0c52f08cacbc913dbe933ad2958f45e3b441875
MeganFu <meganlfu@gmail.com>
2016-11-17 16:27:43 -0500

    works esssentially, changed so picture matches minute not just second

--- photoData.py
+++ photoData.py
@@ -1,6 +1,8 @@
 import exifread
 import glob
 import datetime
+import sys
+from PIL import Image
 
 # Open image file for reading (binary mode)
 def getTags():
@@ -22,13 +24,14 @@ def parseTags(tags):
 		fileString = str(fileWords)
 		timeString = str(time)
 		#turn strings into list
-		allWordsInTag = fileString.split()
+		#split by apostrophe
+		imgName = fileString.split("'")[1]
 		jankyTime = timeString.split()
-		# take third word which is the image name
-		idimgName = allWordsInTag[2]
 		# third element in time is actual time
 		timeClean = jankyTime[1]
-		cleanTags[idimgName] = timeClean
+		#remove seconds from cleanTime
+		timeCleanWoSecond = timeClean[:5]
+		cleanTags[timeCleanWoSecond] = imgName
 	return cleanTags
 
 
@@ -38,9 +41,27 @@ def getCurrentTime():
 	time = datetime.datetime.time(datetime.datetime.now())
 	return time
 
-tags = getTags()
-cleanTags = parseTags(tags)
-print cleanTags
+#get current time not including seconds
+def getCurrentTimewOSecond():
+	time = datetime.datetime.time(datetime.datetime.now())
+	strTime = str(time)
+	return strTime[:5]
 
+def main(args):
+    print args
+
+if __name__ == '__main__':
+	main(sys.argv)
+	tags = getTags()
+	cleanTags = parseTags(tags)
+	#current time
+	# time = getCurrentTimewOSecond()
+	# print merp,time
+	time = '07:07'
+	if time in cleanTags:
+		image = Image.open(cleanTags[time])
+		image.show()
+
+	#print cleanTags
 
 #print imageTags
