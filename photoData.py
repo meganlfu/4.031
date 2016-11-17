ab4d6b53bc72363693d3e2bb727c798cea4de3af
MeganFu <meganlfu@gmail.com>
2016-11-17 15:13:38 -0500

    parsing tags works

--- photoData.py
+++ photoData.py
@@ -7,7 +7,7 @@ def getTags():
 	imageTags = {}
 	#get all images + names
 	for f in glob.glob("*.jpg"):
-		fileSchutff = open(f,'rb')
+		fileSchutff = open(f)
 		tags = exifread.process_file(fileSchutff)
 		#save time tag in {image:time}
 		for tag in tags:
@@ -15,14 +15,32 @@ def getTags():
 	# Return Exif tags
 	return imageTags
 
-def parseTags():
-	
+def parseTags(tags):
+	cleanTags = {}
+	for fileWords,time in tags.items():
+		#convert into Sting bc apparently they're not strings?
+		fileString = str(fileWords)
+		timeString = str(time)
+		#turn strings into list
+		allWordsInTag = fileString.split()
+		jankyTime = timeString.split()
+		# take third word which is the image name
+		idimgName = allWordsInTag[2]
+		# third element in time is actual time
+		timeClean = jankyTime[1]
+		cleanTags[idimgName] = timeClean
+	return cleanTags
+
+
 
 #gets current time
 def getCurrentTime():
 	time = datetime.datetime.time(datetime.datetime.now())
 	return time
 
+tags = getTags()
+cleanTags = parseTags(tags)
+print cleanTags
 
 
 #print imageTags
