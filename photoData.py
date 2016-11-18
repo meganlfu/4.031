b02cf42c158f96b8b10311e89efe47eac358cb79
MeganFu <meganlfu@gmail.com>
2016-11-18 13:24:00 -0500

    added boolean date time that allowed you to choose photos based on data as well

--- photoData.py
+++ photoData.py
@@ -4,6 +4,7 @@ import datetime
 import sys
 from PIL import Image
 
+dateRelevance = True
 # Open image file for reading (binary mode)
 def getTags():
 	imageTags = {}
@@ -17,7 +18,8 @@ def getTags():
 	# Return Exif tags
 	return imageTags
 
-def parseTags(tags):
+def parseTagsTime(tags):
+	'''takes in a dictionary of EXIF tags and returns a dictionary of {(date,time):image name}'''
 	cleanTags = {}
 	for fileWords,time in tags.items():
 		#convert into Sting bc apparently they're not strings?
@@ -27,40 +29,84 @@ def parseTags(tags):
 		#split by apostrophe
 		imgName = fileString.split("'")[1]
 		jankyTime = timeString.split()
-		# third element in time is actual time
+		# second element in time is actual time
 		timeClean = jankyTime[1]
 		#remove seconds from cleanTime
 		timeCleanWoSecond = timeClean[:5]
 		cleanTags[timeCleanWoSecond] = imgName
 	return cleanTags
 
+def parseTagsDateTime(tags):
+	'''takes in a dictionary of EXIF tags and returns a dictionary of {date,time:image name} w/o year or second'''
+	cleanTags = {}
+	for fileWords,dateTime in tags.items():
+		#convert into Sting bc apparently they're not strings?
+		fileString = str(fileWords)
+		timeString = str(dateTime)
+		#turn strings into list
+		#split by apostrophe
+		imgName = fileString.split("'")[1]
+		jankyDateTime = timeString.split()
+		jankyDate = jankyDateTime[0]
+		jankyTime = jankyDateTime[1]
 
+		date = jankyDate[5:]
+		time = jankyTime[:5]
+		# third element in time is actual time
+		# timeClean = jankyTime[1]
+		#remove seconds from cleanTime
+		# timeCleanWoSecond = timeClean[:5]
+		cleanTags[(date,time)] = imgName
+	return cleanTags
 
 #gets current time
 def getCurrentTime():
+	'''returns current time, seconds included'''
 	time = datetime.datetime.time(datetime.datetime.now())
 	return time
 
 #get current time not including seconds
 def getCurrentTimewOSecond():
+	'''returns current time exluding the seconds'''
 	time = datetime.datetime.time(datetime.datetime.now())
 	strTime = str(time)
 	return strTime[:5]
+
+def getCurrentDateandTime():
+	'''returns a tuple with the date (not including year) and time'''
+	time = getCurrentTimewOSecond()
+	dateTime = datetime.datetime.now()
+	justDate = dateTime.strftime("%m-%d")
+	betterDate = justDate.replace ("-", ":")
+	return (betterDate,time)
 
 def main(args):
     print args
 
 if __name__ == '__main__':
+
 	main(sys.argv)
 	tags = getTags()
-	cleanTags = parseTags(tags)
-	#current time
-	# time = getCurrentTimewOSecond()
-	# print merp,time
-	time = '07:07'
-	if time in cleanTags:
-		image = Image.open(cleanTags[time])
-		image.show()
+	#if photo should be specific to date and time
+	if dateRelevance == True:
+		cleanTags = parseTagsDateTime(tags)
+		dateTime = getCurrentDateandTime()
+		print dateTime
+		#if the data and time match, exclude the year
+		dateTime = ('11:14', '21:56')
+		if dateTime in cleanTags:
+			image = Image.open(cleanTags[dateTime])
+			image.show()
+
+	else:
+		cleanTags = parseTagsTime(tags)
+		#current time
+		time = getCurrentTimewOSecond()
+		#for testing
+		time = '07:07'
+		if time in cleanTags:
+			image = Image.open(cleanTags[time])
+			image.show()
 
 	#print cleanTags
 
