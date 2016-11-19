91e2863d45b872703f642869e9c38dbd321bed2c
MeganFu <meganlfu@gmail.com>
2016-11-18 21:24:10 -0500

    added to that it runs every 60 seconds, it works git add photoData.py git add photoData.py

--- photoData.py
+++ photoData.py
@@ -2,19 +2,23 @@ import exifread
 import glob
 import datetime
 import sys
+import time
 from PIL import Image
 
-dateRelevance = True
+dateRelevance = False
+
 # Open image file for reading (binary mode)
 def getTags():
 	imageTags = {}
 	#get all images + names
-	for f in glob.glob("*.jpg"):
+	for f in glob.glob("photos/*.jpg"):
 		fileSchutff = open(f)
 		tags = exifread.process_file(fileSchutff)
 		#save time tag in {image:time}
 		for tag in tags:
 			imageTags[fileSchutff] = tags['EXIF DateTimeOriginal']
+		#to subvert to many files open error for python
+		fileSchutff.close()
 	# Return Exif tags
 	return imageTags
 
@@ -80,34 +84,43 @@ def getCurrentDateandTime():
 	betterDate = justDate.replace ("-", ":")
 	return (betterDate,time)
 
-def main(args):
-    print args
-
-if __name__ == '__main__':
-
-	main(sys.argv)
-	tags = getTags()
+def showPhoto(dateRelevance):
 	#if photo should be specific to date and time
+	tags = getTags()
 	if dateRelevance == True:
 		cleanTags = parseTagsDateTime(tags)
 		dateTime = getCurrentDateandTime()
-		print dateTime
 		#if the data and time match, exclude the year
-		dateTime = ('11:14', '21:56')
 		if dateTime in cleanTags:
 			image = Image.open(cleanTags[dateTime])
 			image.show()
+		else:
+			pass
 
 	else:
 		cleanTags = parseTagsTime(tags)
 		#current time
 		time = getCurrentTimewOSecond()
 		#for testing
-		time = '07:07'
 		if time in cleanTags:
 			image = Image.open(cleanTags[time])
 			image.show()
+		else:
+			pass
 
+
+def main(args):
+    print args
+
+if __name__ == '__main__':
+
+	main(sys.argv)
+	#run every 60 seconds
+	while True:
+		showPhoto(dateRelevance)
+		timE = getCurrentTimewOSecond()
+		print timE
+		time.sleep(60)
 	#print cleanTags
 
 #print imageTags
